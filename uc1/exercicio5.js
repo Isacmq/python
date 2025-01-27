72cc84f24a0e33b02015b7ef0015f36ebdb0cf52
Isac Medeiros Queiroz <medeirosisac613@gmail.com>
2025-01-27 16:39:58 -0300

    atualizaçao 10

---
+++ uc1/exercicio5.js
@@ -0,0 +1,21 @@
+// exercicio 5
+const codCategoria = 3
+switch (codCategoria) {
+    case 1:
+        console.log("alimento perecivel")
+        break
+    case 2:
+        console.log("bebida")
+        break
+    case 1:
+        console.log("limpeza")
+        break
+    case 3:
+        console.log("higiene pessoal")
+        break
+    case 4:
+        console.log("categoria invalida")
+        break
+    default:
+            console.log("categoria invalida")
+}
---
+++ uc1/exercicio6.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Digite um número inteiro:"));
+
+if (numero >= 10 && numero <= 20) {
+    console.log("Número dentro do intervalo");
+} else {
+    console.log("Número fora do intervalo");
+}
