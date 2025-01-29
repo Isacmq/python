7295885f7e63fd208c8b1459dde9cc6114c07c25
Isac Medeiros Queiroz <medeirosisac613@gmail.com>
2025-01-29 16:34:13 -0300

    1

---
+++ projetosDaUc1/media.js
@@ -0,0 +1,10 @@
+// Solicita três números ao usuário
+let num1 = parseFloat(prompt("Digite o primeiro número:"));
+let num2 = parseFloat(prompt("Digite o segundo número:"));
+let num3 = parseFloat(prompt("Digite o terceiro número:"));
+
+// Calcula a média
+let media = (num1 + num2 + num3) / 3;
+
+// Exibe o resultado no console
+console.log("A média dos números é:", media);
