8a267f092672ad3169a6e32140150a204eaf87d0
Isac Medeiros Queiroz <medeirosisac613@gmail.com>
2025-01-31 14:49:16 -0300

    for3

---
+++ aula_Dia_31/for2.js
@@ -0,0 +1,6 @@
+let nome, cadastro = "";
+for (let i = 0; i <= 10; i++) {
+    nome = prompt("informe seu nome");
+    cadastro += nome + "<br>";
+}
+document.write(cadastro);
---
+++ aula_Dia_31/list1.js
@@ -0,0 +1,10 @@
+const carros = ["BMW", "Volvo","Celta","Palio","Chevet"];
+let i = 0
+while (i<=carros.length){
+    document.write(carros[i]+"<br>");
+    i++;
+}
+
+
+// document.write("||---||"+"<br>");
+// document.write(carros [2]);
