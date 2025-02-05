390385ac70b2dedf83704b8877cc55086add88e7
Isac Medeiros Queiroz <medeirosisac613@gmail.com>
2025-02-05 13:55:31 -0300

    .

---
+++ aula_Dia_02/demostracao.js
@@ -0,0 +1,17 @@
+function mensagemboasvindas() {
+    console.log("Bem vindo ao nosso curso de programaçao");
+}
+
+mensagemboasvindas();
+let valor1 = 0
+let valor2 = 1
+let soma = valor1 + valor2;
+
+console.log("A soma dos valores é: " + soma);
+
+let amigos = ["a", "b", "c", "d", "f"];
+
+
+for(let i = 0; i<amigos.length;i++){
+    console.log(amigos[i]);
+}
