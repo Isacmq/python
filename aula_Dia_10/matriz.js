daeb298eea89b4d9d21af278ba7dac80dd784a75
Isac Medeiros Queiroz <medeirosisac613@gmail.com>
2025-02-10 14:11:23 -0300

    "ultima aula"

---
+++ aula_Dia_10/matriz.js
@@ -0,0 +1,13 @@
+// matriz => exemplo
+let matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]; 
+console.log(matriz);
+// acessando um valor
+console.log(matriz[0][1]);
+console.log(matriz[0][2]);
+// modificando um valor
+
+
